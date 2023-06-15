70d6b2cbdc9a9e9d685a9eac73d8cf2f46289ca9
seb <contact@smaloron.eu>
2023-06-15 12:34:12 +0200

    persistence des settings

--- src/app/services/settings.service.ts
+++ src/app/services/settings.service.ts
@@ -1,21 +1,48 @@
 import { Injectable } from '@angular/core';
+import { Storage } from '@ionic/storage-angular';
+
+const STORAGE_KEY = 'todoSettings';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SettingsService {
 
-  settings = {
+  defaultSettings = {
     colors: {
       pendingTask: 'secondary',
       doneTask: 'medium'
     }
   }
 
+  settings = {
+    colors: {
+      pendingTask: 'secondary',
+      doneTask: 'medium'
+    }
+  };
+
   colorNamesList = [
     'primary', 'secondary', 'danger', 'warning', 'light', 'medium', 'dark',
     'tertiary', 'success'
   ];
 
-  constructor() { }
+  private store: Storage;
+
+  constructor() {
+    this.store = new Storage();
+    this.store.create();
+    this.settings = this.defaultSettings;
+
+    this.loadSettings();
+  }
+
+  async loadSettings() {
+    const data = await this.store.get(STORAGE_KEY);
+    this.settings = JSON.parse(data) || this.defaultSettings;
+  }
+
+  persist() {
+    this.store.set(STORAGE_KEY, JSON.stringify(this.settings));
+  }
 }
